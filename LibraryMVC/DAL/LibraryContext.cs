378777a3d5de4c1d9d4060ec234470ad8fb03c5b
kam95oleksy <kam95oleksy@gmail.com>
2017-12-29 15:14:30 +0100

    Add some project file

---
+++ LibraryMVC/DAL/LibraryContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LibraryMVC.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LibraryMVC.DAL
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext() : base("LibraryContext")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ LibraryMVC/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryMVC.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int? RootCategoryId { get; set; }
+
+        public virtual Category RootCategory { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
