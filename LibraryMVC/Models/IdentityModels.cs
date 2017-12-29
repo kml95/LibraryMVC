ef6553414213aad3b9bc17c6a3fb5b4cb44255a1
kam95oleksy <kam95oleksy@gmail.com>
2017-12-29 15:31:08 +0100

    Add some models

--- LibraryMVC/Models/IdentityModels.cs
+++ LibraryMVC/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace LibraryMVC.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("LibraryContext", throwIfV1Schema: false)
         {
         }
 
