a68735c9ee3a0118df8b32c69b0cb26a0aa18ec7
Michal <gil.michal94@gmail.com>
2018-01-10 23:37:00 +0100

    Add Cart

---
+++ LibraryMVC/Controllers/CartController.cs
@@ -0,0 +1,46 @@
+﻿using LibraryMVC.DAL;
+using LibraryMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryMVC.Controllers
+{
+    public class CartController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Cart
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddToChart(int id)
+        {
+            Cart cart = Session["Cart"] as Cart;
+            if (cart == null || Session["Cart"] == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            var book = db.Books.Where(a => a.Id == id).First();
+            cart.addBook(book);
+
+            //zwrocic dalej liste ksizek
+            return RedirectToAction("ShowCart");
+        }
+
+        public ActionResult ShowCart()
+        {
+            if (Session["Cart"] == null)
+            {
+                //pusta lista 
+            }
+            Cart cart = Session["Cart"] as Cart;
+            return View(cart);
+        }
+    }
+}
---
+++ LibraryMVC/Models/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryMVC.Models
+{
+    public class Cart
+    {
+        public List<Book> books { get; set; }
+
+        public Cart()
+        {
+            books = new List<Book>();
+        }
+
+        public bool addBook(Book book)
+        {
+            books.Add(book);
+
+            return true;
+        }
+
+        public bool removeBook(Book book)
+        {
+            books.Remove(book);
+
+            return true;
+        }
+    }
+}
