05d4d214c6e6e2f925c4ee11f9c611558b170c29
kam95oleksy <kam95oleksy@gmail.com>
2018-01-14 12:42:10 +0100

    aa

---
+++ LibraryMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using LibraryMVC.DAL;
+using LibraryMVC.Models;
+using LibraryMVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryMVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class EmployeeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var roleId = db.Roles.Where(r => r.Name == "Employee").FirstOrDefault().Id;
+
+            var users = db.Users.ToList();
+            
+            foreach(var user in users)
+            {
+            }
+            
+            
+            
+            
+            //.Select(user => new EmployeeViewModels { Id = user.Id, Email = user.Email, Role = "Employee"}).ToList();
+            //var huj = db.Users.Where(a => a.Roles.Equals("Employee"));
+            return View();
+        }
+    }
+}
