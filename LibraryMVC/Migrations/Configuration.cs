4f98927b3a1dd5da6831fef40debfd2f4c64f1b1
kam95oleksy <kam95oleksy@gmail.com>
2018-03-14 00:17:13 +0100

    Add seed

--- LibraryMVC/Migrations/Configuration.cs
+++ LibraryMVC/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace LibraryMVC.Migrations
 {
+    using LibraryMVC.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,18 +18,59 @@ namespace LibraryMVC.Migrations
 
         protected override void Seed(LibraryMVC.DAL.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Admin" };
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                manager.Create(role);
+            }
+            if (!context.Roles.Any(r => r.Name == "Employee"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Employee" };
+
+                manager.Create(role);
+            }
+            if (!context.Roles.Any(r => r.Name == "Reader"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Reader" };
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "admin@gmail.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "admin@gmail.com" };
+
+                manager.Create(user, "Password123#");
+                manager.AddToRole(user.Id, "Admin");
+            }
+            if (!context.Users.Any(u => u.UserName == "employee"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "employee" };
+
+                manager.Create(user, "Employee123#");
+                manager.AddToRole(user.Id, "Employee");
+            }
+            if (!context.Users.Any(u => u.UserName == "reader"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "reader" };
+
+                manager.Create(user, "Reader123#");
+                manager.AddToRole(user.Id, "Reader");
+            }
+            
         }
     }
 }
